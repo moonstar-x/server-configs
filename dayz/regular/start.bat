84ab99b9aec4726a03c0d31ef5ff3d104b3479e1
moonstar-x <christian.lopez99@outlook.com>
2023-06-11 15:47:26 -0500

    Updated dayz.

--- dayz/regular/start.bat
+++ dayz/regular/start.bat
@@ -1,8 +1,8 @@
 @echo off
 
-cd ../..
-
 set CURRENT_DIR=%cd%
-set CONFIG_FILE=%CURRENT_DIR%\scripts\regular\server.cfg
+set CONFIG_FILE=%CURRENT_DIR%\server.cfg
+
+cd ../..
 
 DayZServer_x64.exe -name=server -profiles=profiles -config=%CONFIG_FILE% -doLogs -adminLog -netLog -freezeCheck -cpuCount=4
